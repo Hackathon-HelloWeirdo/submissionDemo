bbe6d184120cbc674ae1457f4e755701d26aa421
wzhqwq <wzhii345@outlook.com>
2020-11-29 04:20:17 +0800

    恢复原项目

--- src/main/java/com/jiayou/demo/repository/ReqGoodsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jiayou.demo.repository;
-
-import com.jiayou.demo.entity.ReqGoods;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReqGoodsRepository extends JpaRepository<ReqGoods,Long> {
-}
--- src/main/java/com/jiayou/demo/repository/SupGoodsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jiayou.demo.repository;
-
-import com.jiayou.demo.entity.SupGoods;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupGoodsRepository extends JpaRepository<SupGoods,Long> {
-}
